09cd5e15312dac73d64a38a2a4ae49dbea842c0d
Adric Net <adric@adric.net>
2017-02-21 17:56:10 -0500

    Update entrypoint.sh
    Munging in some change to entrypoint that almost work, ref: https://github.com/google/timesketch/wiki/Installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,17 @@
 set -e
 
 SECRET=$(openssl rand -base64 32)
+PORT=9200
 
 sed -i -e "s#SECRET_KEY = u''#SECRET_KEY = u'${SECRET}'#" /etc/timesketch.conf
 sed -i -e "s#ELASTIC_HOST = u'.*'#ELASTIC_HOST = u'${HOST}'#" /etc/timesketch.conf
 sed -i -e "s#ELASTIC_PORT = .*#ELASTIC_PORT = ${PORT}#" /etc/timesketch.conf
+
+echo "local   all             timesketch                              md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+
+service postgresql restart
+
+sudo -u postgres createuser -d -P -R -S timesketch
+sudo -u postgres createdb -O timesketch timesketch
 
 tsctl runserver -h 0.0.0.0 -p 5000
